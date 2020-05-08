8cd2f952569ca28bcf6a7576696522ee3be4c754
ArgorMaximus <bnsiddhanth0@gmail.com>
2020-05-08 11:57:52 +0530

    second commmit

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "process.env.POSTGRES_USERNAME",
-    "password": "process.env.POSTGRES_PASSWORD",
-    "database": "process.env.POSTGRES_DATABASE",
-    "host": "process.env.POSTGRES_HOST",
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DATABASE,
+    "host": process.env.POSTGRES_HOST,
     "dialect": "postgres",
-    "aws_region": "process.env.AWS_REGION",
-    "aws_profile": "process.env.AWS_PROFILE",
-    "aws_media_bucket": "process.env.AWS_MEDIA_BUCKET"
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
   },
   "prod": {
     "username": "",
